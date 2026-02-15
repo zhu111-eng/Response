c49888408a32ee93da1b41b83e4bd55e48199c6b
linying zhu <75213564+zhu111-eng@users.noreply.github.com>
2026-02-15 12:50:17 +0800

    test: add safety datasets and evaluation metrics

---
+++ src/__init__.py
@@ -0,0 +1,21 @@
+from .bidirectional_attribution import (
+    AttentionRecord,
+    AttributionResult,
+    BidirectionalAttributionEngine,
+    ProcessAttributionAnalyzer,
+    PromptConditionModeler,
+    ResultEvaluator,
+    SimpleNSFWClassifier,
+    ViolationRegionLocalizer,
+)
+
+__all__ = [
+    "AttentionRecord",
+    "AttributionResult",
+    "BidirectionalAttributionEngine",
+    "ProcessAttributionAnalyzer",
+    "PromptConditionModeler",
+    "ResultEvaluator",
+    "SimpleNSFWClassifier",
+    "ViolationRegionLocalizer",
+]
